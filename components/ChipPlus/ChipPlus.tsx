4d68dca0bf6d0672949ceefcb80232ff3eaadd52
Ahn jisoo <anjisu2001@naver.com>
2023-12-19 20:23:57 +0900

    feat: chip 컴포넌트들 구현

---
+++ components/ChipPlus/ChipPlus.tsx
@@ -0,0 +1,12 @@
+import style from "./ChipPlus.module.css";
+const ChipPlus = () => {
+  return (
+    <>
+      <div className={`${style.imageWrapper}`}>
+        <img src="/add.svg" />
+      </div>
+    </>
+  );
+};
+
+export default ChipPlus;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import ChipColors from "@/components/ChipColors/ChipColors";
+import ChipNum from "@/components/ChipNum/ChipNum";
+import ChipPlus from "@/components/ChipPlus/ChipPlus";
+import ChipTag from "@/components/ChipTag/ChipTag";
+import ChipTodo from "@/components/ChipTodo/ChipTodo";
+
 export default function Home() {
-  return <h1 style={{ color: "var(--red)" }}>안녕하세요</h1>;
+  return (
+    <>
+      <ChipNum num="1" />
+      <ChipPlus />
+      <ChipTodo name="On Progress" size="large" />
+      {/* <ChipTag name="백엔드" /> */}
+      <ChipTag name="백엔드" />
+      <ChipColors />
+    </>
+  );
 }
