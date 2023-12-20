caedde678b9e37d76b7505354cd893033a0c7ed3
Haneul-Kim <4188989@naver.com>
2023-12-20 17:50:12 +0900

    fix: 간단한 css 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,22 @@ import Sidemenu from "@/components/sidemenu/Sidemenu";
 
 export default function Home() {
   return (
-    <div style={{ display: "grid", gridTemplateAreas: "'sidemenu header' 'sidemenu main" }}>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateAreas: "'sidemenu header' 'sidemenu main",
+        gridTemplateRows: "auto 1fr",
+      }}
+    >
       <div style={{ gridArea: "sidemenu" }}>
         <Sidemenu />
       </div>
       <div style={{ gridArea: "header" }}>
         <Header />
       </div>
-      <div style={{ gridArea: "main" }}>
+      <div
+        style={{ gridArea: "main", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "6rem" }}
+      >
         <main>메인컨텐츠</main>
       </div>
     </div>
