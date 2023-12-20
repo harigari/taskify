6a8878bfcba5284dac3c3701fa6193ca59c4bc67
Haneul-Kim <4188989@naver.com>
2023-12-20 17:46:28 +0900

    feat (index.tsx) : sidemenu, header 레이아웃 구성

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import Sidemenu from "@/components/sidemenu/Sidemenu";
 
 export default function Home() {
   return (
-    <div style={{ display: "flex" }}>
-      <div>
+    <div style={{ display: "grid", gridTemplateAreas: "'sidemenu header' 'sidemenu main" }}>
+      <div style={{ gridArea: "sidemenu" }}>
         <Sidemenu />
       </div>
-      <div>
+      <div style={{ gridArea: "header" }}>
         <Header />
+      </div>
+      <div style={{ gridArea: "main" }}>
+        <main>메인컨텐츠</main>
       </div>
     </div>
   );
