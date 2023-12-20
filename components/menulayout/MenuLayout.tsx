0b32b597cd430be5227bb5476af4227e1b2371ca
Haneul-Kim <4188989@naver.com>
2023-12-20 17:56:57 +0900

    feat (MenuLayout.tsx) : Sidemenu, Header 레이아웃 컴포넌트

---
+++ components/menulayout/MenuLayout.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/header/Header";
+import Sidemenu from "@/components/sidemenu/Sidemenu";
+import { ReactNode } from "react";
+import styles from "./MenuLayout.module.css";
+import { clsx } from "clsx";
+
+interface Props {
+  children: ReactNode;
+}
+
+const MenuLayout = ({ children }: Props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.grid__sidemenu}>
+        <Sidemenu />
+      </div>
+      <div className={styles.grid__header}>
+        <Header />
+      </div>
+      <div className={clsx(styles.grid__main, styles.main)}>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default MenuLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,5 @@
-import Header from "@/components/header/Header";
-import Sidemenu from "@/components/sidemenu/Sidemenu";
+import MenuLayout from "@/components/menulayout/MenuLayout";
 
 export default function Home() {
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateAreas: "'sidemenu header' 'sidemenu main",
-        gridTemplateRows: "auto 1fr",
-      }}
-    >
-      <div style={{ gridArea: "sidemenu" }}>
-        <Sidemenu />
-      </div>
-      <div style={{ gridArea: "header" }}>
-        <Header />
-      </div>
-      <div
-        style={{ gridArea: "main", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "6rem" }}
-      >
-        <main>메인컨텐츠</main>
-      </div>
-    </div>
-  );
+  return <MenuLayout>메인컨텐츠</MenuLayout>;
 }
