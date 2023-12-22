4cf7adae68981bcf283706e54dd677565464f32c
youdame <112458620+youdame@users.noreply.github.com>
2023-12-22 10:21:17 +0900

    refactor(button.tsx) : 버튼 disabled 속성에 따른 css 코드 변경

--- components/buttons/ArrowButton/ArrowButton.tsx
+++ components/buttons/ArrowButton/ArrowButton.tsx
@@ -13,14 +13,9 @@ const ArrowButton = ({ disabled = false, right = false, onClick }: Props) => {
   const buttonStyle = clsx(styles.button, right && styles.rotate);
 
   return (
-    <button className={clsx(buttonStyle)} onClick={onClick}>
+    <button disabled={disabled} className={clsx(buttonStyle)} onClick={onClick}>
       <div className={styles.icon_wrapper}>
-        <Image
-          className={clsx(disabled && styles.disabled)}
-          alt={`${right ? "오른쪽" : "왼쪽"} 화살표`}
-          fill
-          src="/images/icons/arrow.svg"
-        />
+        <Image alt={`${right ? "오른쪽" : "왼쪽"} 화살표`} fill src="/images/icons/arrow.svg" />
       </div>
     </button>
   );
