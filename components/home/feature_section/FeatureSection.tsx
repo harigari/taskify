35b4ae08e6306b424b8991ae3e78140ebddad002
youdame <112458620+youdame@users.noreply.github.com>
2023-12-23 17:47:25 +0900

    refactor : 파일 분리

---
+++ components/home/feature_section/FeatureSection.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./FeatureSection.module.css";
+import clsx from "clsx";
+import Image from "next/image";
+const FeatureSection = () => {
+  return (
+    <>
+      {/* 두 번째 section */}
+      <section className={styles.feature_section}>
+        <article className={styles.feature_article}>
+          <div className={styles.feature_intro}>
+            <span className={styles.feature_number}>Point 1</span>
+            <h2 className={clsx(styles.heading, styles.feature_h2)}>
+              일의 우선순위를 <br />
+              관리하세요
+            </h2>
+          </div>
+          <div className={styles.flex_box1}>
+            <div className={styles.feature_image_wrapper1}>
+              <Image fill alt="우선순위 관리 기능 예시 이미지" src="/images/photos/landing_feature1.png" />
+            </div>
+          </div>
+        </article>
+
+        <article className={clsx(styles.feature_article, styles.reverse)}>
+          <div className={styles.feature_intro}>
+            <span className={styles.feature_number}>Point 2</span>
+            <h2 className={clsx(styles.heading, styles.feature_h2)}>
+              해야 할 일을 <br />
+              등록하세요
+            </h2>
+          </div>
+          <div className={styles.flex_box2}>
+            <div className={styles.feature_image_wrapper2}>
+              <Image fill alt="할 일 생성 기능 예시 이미지" src="/images/photos/landing_feature2.png" />
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default FeatureSection;
---
+++ components/home/setting_section/SettingSection.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./SettingSection.module.css";
+import clsx from "clsx";
+import Image from "next/image";
+
+interface SettingData {
+  id: number;
+  alt: string;
+  imageSrc: string;
+  heading: string;
+  description: string;
+}
+
+const settingData: SettingData[] = [
+  {
+    id: 1,
+    alt: "대시 보드 설정 이미지",
+    imageSrc: "/images/photos/landing_setting1.png",
+    heading: "대시보드 설정",
+    description: "대시보드 사진과 이름을 변경할 수 있어요.",
+  },
+  {
+    id: 2,
+    alt: "새로운 팀원 초대 이미지",
+    imageSrc: "/images/photos/landing_setting2.png",
+    heading: "초대",
+    description: "새로운 팀원을 초대할 수 있어요.",
+  },
+  {
+    id: 3,
+    alt: "구성원 초대 이미지",
+    imageSrc: "/images/photos/landing_setting3.png",
+    heading: "구성원",
+    description: "구성원을 초대하고 내보낼 수 있어요.",
+  },
+];
+
+const SettingArticle = ({ alt, imageSrc, heading, description }: SettingData) => (
+  <>
+    <article className={clsx(styles.setting_article)}>
+      <div className={styles.setting_image_wrapper}>
+        <Image fill alt={alt} src={imageSrc} />
+      </div>
+      <div className={styles.setting_description}>
+        <h3 className={clsx(styles.heading, styles.setting_h3)}>{heading}</h3>
+        <span>{description}</span>
+      </div>
+    </article>
+  </>
+);
+
+const SettingSection = () => (
+  <section className={clsx(styles.setting_section)}>
+    <h2 className={clsx(styles.heading, styles.setting_h2)}>생산성을 높이는 다양한 설정 ⚡️</h2>
+    <div className={clsx(styles.setting_container)}>
+      {settingData.map((data) => (
+        <SettingArticle key={data.id} {...data} />
+      ))}
+    </div>
+  </section>
+);
+
+export default SettingSection;
