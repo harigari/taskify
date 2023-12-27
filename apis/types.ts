029a1023ec19fd6953f4362a3ae48e94386a346b
ayden94 <144667387+ayden94@users.noreply.github.com>
2023-12-27 23:22:23 +0900

    feat(types.ts) : api 통신을 통해 리턴되는 타입들 정리

--- apis/types.ts
+++ apis/types.ts
@@ -1,19 +1,22 @@
-interface TimeStamp {
+type TimeStamp = {
   createdAt?: Date;
   updatedAt?: Date;
-}
+};
 
+// 유저 관련 타입
 type BasicUserType = {
   profileImageUrl: string | null;
   nickname: string;
   id: number;
 };
 
-export interface UserType extends TimeStamp, BasicUserType {
+type ExtendedUserType = {
   email: string;
-}
+} & TimeStamp &
+  BasicUserType;
 
-export interface CardType extends TimeStamp {
+// 카드 관련 타입
+type CardType = {
   id: number;
   title: string;
   description: string;
@@ -24,30 +27,69 @@ export interface CardType extends TimeStamp {
   teamId: string;
   columnId: number;
   dashboardId: number;
-}
+} & TimeStamp;
 
-export interface columnType extends TimeStamp {
+type CardListType = {
+  cursorId: 0;
+  totalCount: 0;
+  cards: CardType[];
+};
+
+// 컬럼 관련 타입
+type ColumnType = {
   id: number;
-  content: string;
   cardId: number;
   author: BasicUserType;
-}
+} & TimeStamp;
 
-export interface ImageUrlType {
+type ColumnListType = {
+  result: "SUCCESS";
+  data: ColumnType[];
+};
+
+type ImageUrlType = {
   imageUrl: string;
-}
+};
 
-export interface CommentType extends TimeStamp {
+// 댓글 관련 타입
+type CommentType = {
   id: number;
   content: string;
   cardId: number;
   author: BasicUserType;
-}
+} & TimeStamp;
 
-export interface DashBoardType extends TimeStamp {
+type CommentListType = {
+  cursorId: 0;
+  comments: CommentType[];
+};
+
+// 대시보드 관련 타입
+type DashBoardType = {
   id: number;
   title: string;
   color: string;
   createdByMe: Boolean;
   userId: number;
-}
+} & TimeStamp;
+
+type DashBoardListType = {
+  cursorId: 0;
+  totalCount: 0;
+  dashboards: DashBoardType[];
+};
+
+// 대시보드 맴버 관련 타입
+type Member = {
+  id: number;
+  userId: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  isOwner: boolean;
+} & TimeStamp;
+
+type DashBoardMemberType = {
+  members: Member[];
+  totalCount: 0;
+};
