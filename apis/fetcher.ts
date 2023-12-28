5c987e6a246d877f50cbcc5eff62a7d2c1b585ca
ayden94 <144667387+ayden94@users.noreply.github.com>
2023-12-28 13:25:21 +0900

    refactor(fetcher) : 변수명 변경

--- apis/fetcher.ts
+++ apis/fetcher.ts
@@ -1,14 +1,14 @@
 import apiConfig from "./apiConfigs";
 
-const fetcher = async (path: string, option: { header; method; body }) => {
+const fetcher = async <T>(path: string, option: RequestInit): Promise<T | undefined> => {
   const BASE_URL = apiConfig.BASE_URL;
   const res = await fetch(`${BASE_URL}${path}`, option);
 
   try {
-    const data = res.json();
-    return data;
+    const result: T = await res.json();
+    return result;
   } catch (error) {
-    console.log(error);
+    throw new Error("Failed to parse JSON");
   }
 };
 
