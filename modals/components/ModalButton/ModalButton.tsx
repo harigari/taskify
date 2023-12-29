f47b789f2ec7014bd869ab74d903a57c253c1f85
ayden94 <144667387+ayden94@users.noreply.github.com>
2023-12-29 14:56:40 +0900

    feat(ModalButton.tsx) : disabled를 받을 수 있게 함

--- modals/components/ModalButton/ModalButton.tsx
+++ modals/components/ModalButton/ModalButton.tsx
@@ -2,33 +2,62 @@ import clsx from "clsx";
 import styles from "./ModalButton.module.css";
 import { ReactNode } from "react";
 
-function ModalSubmit({ children, className }: { children: ReactNode; className?: string }) {
+function ModalSubmit({
+  children,
+  className,
+  disabled = false,
+}: {
+  children: ReactNode;
+  className?: string;
+  disabled?: boolean;
+}) {
   const singleButtonStyle = clsx(styles.root, styles.single, className);
 
   return (
-    <button type="submit" className={singleButtonStyle}>
+    <button type="submit" disabled={disabled} className={singleButtonStyle}>
       {children}
     </button>
   );
 }
 
-function SingleButton({ children, onClick }: { children: ReactNode; onClick: () => void }) {
+function SingleButton({
+  children,
+  onClick,
+  disabled = false,
+}: {
+  children: ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+}) {
   return (
     <div className={styles.buttonContainer}>
-      <button type="button" onClick={onClick} className={`${styles.root} ${styles.double} ${styles.accept}`}>
+      <button
+        type="button"
+        disabled={disabled}
+        onClick={onClick}
+        className={`${styles.root} ${styles.double} ${styles.accept}`}
+      >
         {children}
       </button>
     </div>
   );
 }
 
-function DoubleButton({ children, onClick }: { children: ReactNode; onClick: () => void }) {
+function DoubleButton({
+  children,
+  onClick,
+  disabled = false,
+}: {
+  children: ReactNode;
+  onClick: () => void;
+  disabled: boolean;
+}) {
   return (
     <div className={styles.buttonContainer}>
       <button onClick={onClick} type="button" className={`${styles.root} ${styles.double} ${styles.cancel}`}>
         취소
       </button>
-      <button type="submit" className={`${styles.root} ${styles.double} ${styles.accept}`}>
+      <button type="submit" disabled={disabled} className={`${styles.root} ${styles.double} ${styles.accept}`}>
         {children}
       </button>
     </div>
